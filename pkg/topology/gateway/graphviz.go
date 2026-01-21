31d67f2103159630da6f749a0cbff483d82e88df
audrastump <a.e.stump@wustl.edu>
2026-01-21 10:42:57 -0800

    Use DOT subgraph clusters to represent namespaces
    Signed-off-by: audrastump <a.e.stump@wustl.edu>

--- pkg/topology/gateway/graphviz.go
+++ pkg/topology/gateway/graphviz.go
@@ -18,6 +18,7 @@ package gateway
 
 import (
 	"github.com/emicklei/dot"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"sigs.k8s.io/gwctl/pkg/common"
 	"sigs.k8s.io/gwctl/pkg/topology"
 )
@@ -28,14 +29,52 @@ import (
 func ToDot(gwctlGraph *topology.Graph) (string, error) {
 	dotGraph := dot.NewGraph(dot.Directed)
 	dotGraph.Attr("rankdir", "BT")
+	dotGraph.Attr("compound", "true")
+
+	// Collect all unique namespaces from nodes
+	namespaces := map[string]bool{}
+	for _, nodeMap := range gwctlGraph.Nodes {
+		for _, node := range nodeMap {
+			// Skip Namespace nodes - they will be represented as clusters
+			if node.GKNN().GroupKind() == common.NamespaceGK {
+				continue
+			}
+			if ns := node.GKNN().Namespace; ns != "" {
+				namespaces[ns] = true
+			}
+		}
+	}
+
+	// Create subgraphs for each namespace
+	clusterMap := map[string]*dot.Graph{}
+	for ns := range namespaces {
+		cluster := dotGraph.Subgraph("cluster_"+ns, dot.ClusterOption{})
+		cluster.Attr("label", "Namespace: "+ns)
+		cluster.Attr("style", "filled")
+		cluster.Attr("color", mapColor(common.NamespaceGK))
+		cluster.Attr("fillcolor", mapColor(common.NamespaceGK)+"33")
+		clusterMap[ns] = cluster
+	}
 
 	// Create nodes.
 	dotNodeMap := map[common.GKNN]dot.Node{}
 	for _, nodeMap := range gwctlGraph.Nodes {
 		for _, node := range nodeMap {
-			dotNode := dotGraph.Node(node.GKNN().String()).
+
+			if node.GKNN().GroupKind() == common.NamespaceGK {
+				continue
+			}
+
+			var targetGraph *dot.Graph
+			if ns := node.GKNN().Namespace; ns != "" {
+				targetGraph = clusterMap[ns]
+			} else {
+				targetGraph = dotGraph
+			}
+
+			dotNode := targetGraph.Node(node.GKNN().String()).
 				Attr("style", "filled").
-				Attr("color", mapNodeColor(node))
+				Attr("color", mapColor(node.GKNN().GroupKind()))
 
 			dotNodeMap[node.GKNN()] = dotNode
 
@@ -44,10 +83,8 @@ func ToDot(gwctlGraph *topology.Graph) (string, error) {
 			if gk.Group == common.GatewayGK.Group {
 				gk.Group = ""
 			}
-			name := node.GKNN().NamespacedName().String()
-			if node.GKNN().Namespace == "" {
-				name = node.GKNN().Name
-			}
+
+			name := node.GKNN().Name
 			dotNode.Label(gk.String() + "\n" + name)
 		}
 	}
@@ -58,6 +95,11 @@ func ToDot(gwctlGraph *topology.Graph) (string, error) {
 
 		for relation, outNodeMap := range fromNode.OutNeighbors {
 			for toNodeGKNN := range outNodeMap {
+				// Skip edges to Namespace nodes - namespace relationship are represented by cluster membership
+				if toNodeGKNN.GroupKind() == common.NamespaceGK {
+					continue
+				}
+
 				dotToNode := dotNodeMap[toNodeGKNN]
 
 				// If this is an edge from an HTTPRoute to a Service, then
@@ -65,8 +107,7 @@ func ToDot(gwctlGraph *topology.Graph) (string, error) {
 				// then reverse the display again to show the correct direction.
 				// The end result being that Services now get assigned the
 				// correct rank.
-				reverse := (fromNode.GKNN().GroupKind() == common.HTTPRouteGK && toNodeGKNN.GroupKind() == common.ServiceGK) ||
-					(fromNode.GKNN().GroupKind() == common.GatewayGK && toNodeGKNN.GroupKind() == common.NamespaceGK)
+				reverse := fromNode.GKNN().GroupKind() == common.HTTPRouteGK && toNodeGKNN.GroupKind() == common.ServiceGK
 				u, v := dotFromNode, dotToNode
 				if reverse {
 					u, v = v, u
@@ -77,10 +118,6 @@ func ToDot(gwctlGraph *topology.Graph) (string, error) {
 				if reverse {
 					e.Attr("dir", "back")
 				}
-				// Create a dotted line for the relation to the namespace.
-				if toNodeGKNN.Kind == common.NamespaceGK.Kind {
-					e.Dotted()
-				}
 			}
 		}
 	}
@@ -88,8 +125,8 @@ func ToDot(gwctlGraph *topology.Graph) (string, error) {
 	return dotGraph.String(), nil
 }
 
-func mapNodeColor(node *topology.Node) string {
-	switch node.GKNN().GroupKind() {
+func mapColor(gk schema.GroupKind) string {
+	switch gk {
 	case common.NamespaceGK:
 		return "#d08770"
 	case common.GatewayClassGK:
